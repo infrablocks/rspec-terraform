ab7b9d36f8597896e683b5c60c3805a8920bb42c
Toby <tobyclemson@gmail.com>
2022-08-09 18:14:14 +0100

    WIP

---
+++ lib/rspec/terraform/matchers/include_output_change.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module RSpec
+  module Terraform
+    module Matchers
+      class IncludeOutputChange
+        attr_reader :definition
+
+        def initialize(definition)
+          @definition = definition
+        end
+
+        def matches?(plan)
+          !definition_matches(plan).empty?
+        end
+
+        def with_value(i)
+          self
+        end
+
+        private
+
+        def definition_matches(plan)
+          plan.output_changes_matching(definition)
+        end
+      end
+    end
+  end
+end
